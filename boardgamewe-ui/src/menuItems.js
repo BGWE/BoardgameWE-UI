db201c6e705609be4106775e19d8e19308c47139
Fabrice Servais <fabrice.servais@gmail.com>
2018-05-13 14:33:29 +0200

    Add players item in menu

--- boardgamewe-ui/src/menuItems.js
+++ boardgamewe-ui/src/menuItems.js
@@ -4,17 +4,24 @@ import {Link} from "react-router-dom";
 
 export const menuItems = (
     <div>
-        <Link to="/">
+        <Link style={{ textDecoration: 'none' }} to="/">
             <ListItem button>
                 <ListItemText primary="Home" />
             </ListItem>
         </Link>
 
-        <Link to="/boardgames">
+        <Link style={{ textDecoration: 'none' }} to="/boardgames">
             <ListItem button>
                  <ListItemText primary="Board Games" />
             </ListItem>
         </Link>
+
+        <Link style={{ textDecoration: 'none' }} to="/players">
+            <ListItem button>
+                <ListItemText primary="Players" />
+            </ListItem>
+        </Link>
+
         <ListItem button>
             <ListItemText primary="Games" />
         </ListItem>
