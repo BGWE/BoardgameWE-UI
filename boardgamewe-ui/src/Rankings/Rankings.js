0c474002897bb7fb8ac4fc84763fdc49c5f7fc34
Romain Mormont <romainmormont@hotmail.com>
2018-05-17 00:41:55 +0200

    add ranking table component

--- boardgamewe-ui/src/Rankings/Rankings.js
+++ boardgamewe-ui/src/Rankings/Rankings.js
@@ -16,6 +16,7 @@ import {
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import classNames from 'classnames';
 import RankingCard from "./RankingCard";
+import RankingTable from "./RankingTable";
 
 
 // const styles = theme => ({
@@ -79,19 +80,69 @@ class Rankings extends React.Component {
 
         this.state = {
             spacing: '40',
-
+            rankings: null,
+            isLoading: true
         };
 
     }
 
     componentDidMount() {
-
+        this.setState({ isLoading: true });
+        this.reload();
     }
 
+    getRankingBest (ranking_name) {
+        if (!this.state.rankings || this.state.rankings[ranking_name].length === 0) {
+            return "/";
+        }
+
+        const best = this.state.rankings[ranking_name][0];
+        return best.player.name + " - " + best.score;
+    }
+
+    getRankingTableOrProgress (ranking_name, classes) {
+        if (!this.state.rankings) {
+            return (
+                <div className={classes.root}>
+                    <CircularProgress thickness={7} />
+                </div>
+            );
+        }
+
+        return (
+            <RankingTable ranking={this.state.rankings[ranking_name]}>
+            </RankingTable>
+        );
+    }
+
+    reload() {
+        fetch('http://api.boardgameweekend.party/stats/rankings/')
+            .then(response => response.json())
+            .then(function (data) {
+                console.log("request");
+                console.log(data);
+                this.setState({ rankings: data, isLoading: false })
+            }.bind(this))
+            .catch(error => {
+                console.log(error);
+                this.setState({ snackbar_error: true, isLoading: false })
+            });
+    }
 
     render () {
         const { classes } = this.props;
         const { spacing } = this.state;
+
+        console.log(this.state);
+
+        const rankings_info = [
+            { ranking_name: "victory_count", title: "Number of games won" },
+            { ranking_name: "defeat_count", title: "Number of games lost" },
+            { ranking_name: "victory_prop", title: "Percentage of games won" },
+            { ranking_name: "defeat_prop", title: "Percentage of games lost" },
+            { ranking_name: "count_games", title: "Number of games played" },
+            { ranking_name: "count_unique_games", title: "Number of different games played" }
+        ];
 
         return (
             <div className={classes.root} style={{backgroundColor: '#fafafa'}}>
@@ -102,30 +153,15 @@ class Rankings extends React.Component {
                     <Grid container className={classes.root} spacing={16}>
                         <Grid item xs>
                             <Grid container className={classes.demo} justify="center" spacing={Number(spacing)}>
-
-                                <Grid key="n_games_played" item className={classes.grid_card}>
-                                    <RankingCard
-                                        title="Number of games played"
-                                        value="30">
-                                        DETAILS
-                                    </RankingCard>
-                                </Grid>
-
-                                <Grid key="n_games_won" item className={classes.grid_card}>
-                                    <RankingCard
-                                        title="Number of games won"
-                                        value="30">
-                                        DETAILS
-                                    </RankingCard>
-                                </Grid>
-
-                                <Grid key="n_games_lost" item className={classes.grid_card}>
-                                    <RankingCard
-                                        title="Number of games lost"
-                                        value="30">
-                                        DETAILS
-                                    </RankingCard>
-                                </Grid>
+                                {rankings_info.map(info => (
+                                    <Grid key={info.ranking_name} item className={classes.grid_card}>
+                                        <RankingCard
+                                            title={info.title}
+                                            value={this.getRankingBest(info.ranking_name)}>
+                                            {this.getRankingTableOrProgress(info.ranking_name, classes)}
+                                        </RankingCard>
+                                    </Grid>
+                                ))}
                             </Grid>
                         </Grid>
                     </Grid>
