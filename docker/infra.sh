e2c2b12e055cdc25f91eb5fb6ab53f5dfba6c431
Fabrice Servais <fabrice.servais@gmail.com>
2018-10-09 19:33:03 +0200

    Fix FServais/BoardgameWE#11 - App is containerized to run locally

---
+++ docker/infra.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+SCRIPTDIR="$(dirname "$(which "$0")")"
+while test $# -gt 0
+do
+    case "$1" in
+        start) docker-compose -f $SCRIPTDIR/docker-compose.yml -p bgwe_frontend build && docker-compose -f $SCRIPTDIR/docker-compose.yml -p bgwe_frontend up -d --remove-orphans
+            ;;
+        stop) docker-compose -f $SCRIPTDIR/docker-compose.yml -p bgwe_frontend stop ; docker-compose -f $SCRIPTDIR/docker-compose.yml -p bgwe_frontend rm -f
+            ;;
+        reload) docker-compose -f $SCRIPTDIR/docker-compose.yml -p bgwe_frontend stop ; docker-compose -f $SCRIPTDIR/docker-compose.yml -p bgwe_frontend rm -f ; docker-compose -f $SCRIPTDIR/docker-compose.yml -p bgwe_frontend build && docker-compose -f $SCRIPTDIR/docker-compose.yml -p bgwe_frontend up -d --remove-orphans
+        *) echo "bad option $1"
+    esac
+    shift
+done
