aa78887a0063f8747b4c6d4a25c23f2585caa64e
Fabrice Servais <fabrice.servais@gmail.com>
2018-10-19 23:46:47 +0200

    Fix FServais/BoardgameWE#27 - List of events.

--- boardgamewe-ui/src/Events/EventsLayout.js
+++ boardgamewe-ui/src/Events/EventsLayout.js
@@ -22,14 +22,6 @@ import Button from "@material-ui/core/Button/Button";
 import EventIcon from '@material-ui/icons/Event';
 import CardHeader from "@material-ui/core/CardHeader/CardHeader";
 
-// const styles = theme => ({
-//     root: {
-//         width: '100%',
-//         maxWidth: 360,
-//         backgroundColor: theme.palette.background.paper,
-//     },
-// });
-
 const styles = theme => ({
     appBar: {
         position: 'relative',
@@ -127,27 +119,6 @@ class EventsLayout extends React.Component {
         const { classes } = this.props;
         return (
             <div className={classes.root}>
-                {/*<List component="nav">*/}
-                    {/*<Link to="/event/1">*/}
-                        {/*<ListItem button>*/}
-                            {/*<ListItemText primary="Event 1" />*/}
-                        {/*</ListItem>*/}
-                    {/*</Link>*/}
-
-                    {/*<ListItem button>*/}
-                        {/*<ListItemText primary="Event 2" />*/}
-                    {/*</ListItem>*/}
-                {/*</List>*/}
-                {/*<Divider />*/}
-                {/*<List component="nav">*/}
-                    {/*<ListItem button>*/}
-                        {/*<ListItemText primary="Trash" />*/}
-                    {/*</ListItem>*/}
-                    {/*<ListItem button component="a" href="#simple-list">*/}
-                        {/*<ListItemText primary="Spam" />*/}
-                    {/*</ListItem>*/}
-                {/*</List>*/}
-
                 <React.Fragment>
                     <CssBaseline />
                     <AppBar position="static" className={classes.appBar}>
@@ -187,11 +158,6 @@ class EventsLayout extends React.Component {
                                 {this.state.events.map(event => (
                                     <Grid item key={event.id} sm={6} md={4} lg={3}>
                                         <Card className={classes.card}>
-                                            {/*<CardMedia*/}
-                                                {/*className={classes.cardMedia}*/}
-                                                {/*image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len*/}
-                                                {/*title="Image title"*/}
-                                            {/*/>*/}
                                             <CardHeader
                                                 className={classes.cardHeader}
                                                 title={
@@ -214,9 +180,6 @@ class EventsLayout extends React.Component {
                                                     onClick={() => this.goToEvent(event.id)}>
                                                     Select
                                                 </Button>
-                                                {/*<Button size="small" color="primary">*/}
-                                                    {/*Edit*/}
-                                                {/*</Button>*/}
                                             </CardActions>
                                         </Card>
                                     </Grid>
