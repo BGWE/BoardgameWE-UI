5b0857545e6978d76916abcc4803ed68b79b523e
Romain Mormont <romainmormont@hotmail.com>
2019-03-17 16:06:11 +0100

    implement get user stats request

--- boardgame-ui/src/utils/api/User.js
+++ boardgame-ui/src/utils/api/User.js
@@ -51,6 +51,17 @@ export default class User extends Model {
   }
 
   /**
+   * Fetch user statistics
+   * @param userId (Optional) Identifier of the user (default: user identifier)
+   * @returns {Object} Response from backend
+   */
+  static async fetchStats(userId) {
+    const id = userId || this.id;
+    let {data} = await axios.get(`/user/${id}/stats`);
+    return data;
+  }
+
+  /**
    * [ADMIN] Fetch the listing of users with their status
    * @return {Array} List of users and their status.
    */
