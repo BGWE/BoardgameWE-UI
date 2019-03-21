260498e379737d246a46135dd6b7a22abef2a152
Romain Mormont <romainmormont@hotmail.com>
2019-03-21 23:26:04 +0100

    fix guest home page + add structure for future content

--- boardgame-ui/src/pages/GuestHomePage.vue
+++ boardgame-ui/src/pages/GuestHomePage.vue
@@ -1,11 +1,29 @@
 <template>
-    Hello
+    <div>
+      <section class="hero is-dark">
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title">
+              {{$t('home.hello')}} !
+              <span class="icon">
+            <i class="hero-smile fa fa-smile"></i>
+          </span>
+            </h1>
+            <h2 class="subtitle">
+              {{$t('home.welcome')}} {{$t('app.appName')}}.
+            </h2>
+          </div>
+        </div>
+      </section>
+
+      Lorem ipsum..
+    </div>
 </template>
 
 <script>
 export default {
-    name: "UserHomePage"
-}
+  name: 'GuestHomePage'
+};
 </script>
 
 <style scoped>
