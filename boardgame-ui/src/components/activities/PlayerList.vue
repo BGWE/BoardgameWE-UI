47cf186b30e8665a0c8063e0b2c6a0ccbd66dc64
hjwk <hwoszczyk@uliege.be>
2019-05-09 23:04:46 +0200

    Fixed minor bug in PlayerList

--- boardgame-ui/src/components/activities/PlayerList.vue
+++ boardgame-ui/src/components/activities/PlayerList.vue
@@ -4,11 +4,12 @@
       <strong>{{names[0]}}</strong>
     </span>
     <span v-else-if="names.length < 4">
-      <span v-for="name in names.slice(0, -3)" :key="name" ><strong>{{name}}</strong>,&nbsp;</span>
+      <span v-for="name in names.slice(0, -2)" :key="name"> <strong>{{name}}</strong>,&nbsp;</span>
       <strong>{{names[names.length - 2]}}</strong>&nbsp;<i18n path="and"/>&nbsp;<strong>{{names[names.length - 1]}}</strong>
     </span>
     <span v-else>
-      <span v-for="name in names.slice(0, 4)" :key="name" ><strong>{{name}}</strong>,&nbsp;</span>...
+      <span v-for="name in names.slice(0, 3)" :key="name" ><strong>{{name}}</strong>,</span>
+      <strong> {{names[4]}} </strong>...
     </span>
   </span>
 </template>
