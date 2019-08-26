7e91efc658d1f0e69415d98dbccc387ed4a622e2
hjwk <woszczykh@gmail.com>
2019-08-26 17:14:15 +0200

    Fix error "this.userAchievements" is null

--- boardgame-ui/src/components/user/UserAchievements.vue
+++ boardgame-ui/src/components/user/UserAchievements.vue
@@ -1,49 +1,50 @@
 <template>
   <div>
     <b-loading :is-full-page="false" :active="loading" />
-    
-    <div v-for="achievement in this.userAchievements" :key="achievement.id">
-      <div class="achievement-container">
-        <article class="media">
-          <figure class="media-left">
-            <p class="image is-64x64">
-              <img src="@/assets/achievements/placeholder.png">
-            </p>
-          </figure>
-          <div class="media-content">
-            <div class="content">
-              <p>
-                <strong> {{$t(achievement.id_achievement + '.title')}} </strong> <small> @{{formatedDateTime(achievement.createdAt)}} </small>
-                <br>
-                {{$t(achievement.id_achievement + '.descr')}}
+    <template v-if="!loading">
+      <div v-for="achievement in this.userAchievements" :key="achievement.id">
+        <div class="achievement-container">
+          <article class="media">
+            <figure class="media-left">
+              <p class="image is-64x64">
+                <img src="@/assets/achievements/placeholder.png">
               </p>
+            </figure>
+            <div class="media-content">
+              <div class="content">
+                <p>
+                  <strong> {{$t(achievement.id_achievement + '.title')}} </strong> <small> @{{formatedDateTime(achievement.createdAt)}} </small>
+                  <br>
+                  {{$t(achievement.id_achievement + '.descr')}}
+                </p>
+              </div>
             </div>
-          </div>
-        </article>
+          </article>
+        </div>
       </div>
-    </div>
 
-    <div v-if="this.total > this.userAchievements.length" style="padding-top:15px">
-      <div class="achievement-container">
-        <article class="media">
-          <figure class="media-left">
-            <p class="image is-64x64">
-              <img src="@/assets/achievements/locked_achievement.png">
-            </p>
-          </figure>
-          <div class="media-content">
-            <div class="content">
-              <p>
-                <strong> {{$t('achievements.locked')}} </strong>
-                <br>
-                {{$t('achievements.continue')}}
+      <div v-if="this.total > this.userAchievements.length" style="padding-top:15px">
+        <div class="achievement-container">
+          <article class="media">
+            <figure class="media-left">
+              <p class="image is-64x64">
+                <img src="@/assets/achievements/locked_achievement.png">
               </p>
+            </figure>
+            <div class="media-content">
+              <div class="content">
+                <p>
+                  <strong> {{$t('achievements.locked')}} </strong>
+                  <br>
+                  {{$t('achievements.continue')}}
+                </p>
+              </div>
             </div>
-          </div>
-        </article>
+          </article>
+        </div>
       </div>
-    </div>
 
+    </template>
   </div>
 </template>
 
