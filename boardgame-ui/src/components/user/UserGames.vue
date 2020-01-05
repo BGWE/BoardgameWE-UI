fad9e4a0467bedf3f22ef80c03eb509bd49092e4
hjwk <woszczykh@gmail.com>
2020-01-05 17:08:29 +0100

    Simple filtering of games

--- boardgame-ui/src/components/user/UserGames.vue
+++ boardgame-ui/src/components/user/UserGames.vue
@@ -2,8 +2,79 @@
   <div>
     <b-loading :is-full-page="false" :active="loading"/>
     <template v-if="!loading">
-      <div v-if="isCurrentUserProfile" class="columns">
-        <div class="column has-text-right">
+      <div class="columns is-vcentered is-multiline">
+        <div class="column is-narrow">
+          <b-field class="narrow-field">
+            <b-input v-model="nbPlayers" icon="users"></b-input>
+          </b-field>
+        </div>
+        <div class="column is-narrow">
+          <b-field grouped group-multiline>
+            <b-field class="narrow-field">
+              <b-select v-model="day">
+                <option value="All">Day</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+                <option value="13">13</option>
+                <option value="14">14</option>
+                <option value="15">15</option>
+                <option value="16">16</option>
+                <option value="17">17</option>
+                <option value="18">18</option>
+                <option value="19">19</option>
+                <option value="20">20</option>
+                <option value="21">21</option>
+                <option value="22">22</option>
+                <option value="23">23</option>
+                <option value="24">24</option>
+                <option value="25">25</option>
+                <option value="26">26</option>
+                <option value="27">27</option>
+                <option value="28">28</option>
+                <option value="29">29</option>
+                <option value="30">30</option>
+                <option value="31">31</option>
+              </b-select>
+            </b-field>
+            <b-field class="narrow-field">
+              <b-select v-model="month">
+                <option value="All">Month</option>
+                <option value="0">January</option>
+                <option value="1">February</option>
+                <option value="2">March</option>
+                <option value="3">April</option>
+                <option value="4">May</option>
+                <option value="5">June</option>
+                <option value="6">July</option>
+                <option value="7">August</option>
+                <option value="8">September</option>
+                <option value="9">Octobre</option>
+                <option value="10">November</option>
+                <option value="11">December</option>
+              </b-select>
+            </b-field>
+            <b-field class="narrow-field">
+              <b-select v-model="year">
+                <option value="All">Year</option>
+                <option value="2020">2020</option>
+                <option value="2019">2019</option>
+                <option value="2018">2018</option>
+                <option value="2017">2017</option>
+              </b-select>
+            </b-field>
+          </b-field>
+        </div>
+        <div v-if="isCurrentUserProfile" class="column has-text-right">
           <router-link tag="button" class="button is-primary" :to="{name: 'add-game'}">
             {{$t("games.add")}}
           </router-link>
@@ -15,7 +86,7 @@
       </p>
 
       <GamesList v-if="games"
-        :games="games"
+        :games="filteredGames"
         :isCurrentUserProfile="isCurrentUserProfile"
         @gameDeleted="reload()"
       />
@@ -27,6 +98,7 @@
 <script>
 import User from '@/utils/api/User';
 import GamesList from '@/components/games/GamesList';
+import * as Helper from '@/utils/helper';
 
 export default {
   props: {
@@ -41,6 +113,10 @@ export default {
   data() {
     return {
       games: null,
+      nbPlayers: null,
+      day: 'All',
+      month: 'All',
+      year: 'All',
       loading: true
     };
   },
@@ -49,7 +125,25 @@ export default {
     async reload() {
       this.loading = true;
       this.games = await this.user.fetchGames();
-      this.loading = false; 
+      this.loading = false;
+    }
+  },
+
+  computed: {
+    filteredGames() {
+      let nbPlayers = Number(this.nbPlayers);
+      let day = Number(this.day);
+      let month = Number(this.month);
+      let year = Number(this.year);
+
+      return this.games.filter(game => {
+        let date = Helper.ISO8601ToDate(game.createdAt);
+        console.log(date.getMonth());
+        return ( !nbPlayers || game.players.length === nbPlayers ) &&
+        ( isNaN(day) || date.getDate() === day ) &&
+        ( isNaN(month) || date.getMonth() === month ) &&
+        ( isNaN(year) || date.getFullYear() === year );
+      });
     }
   },
 
@@ -58,3 +152,10 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.narrow-field {
+  max-width: 8.3em;
+}
+
+</style>
