fe564341607b419ce5999254bb69c4a2a5702f66
Fabrice Servais <fabrice.servais@gmail.com>
2020-06-22 03:47:30 +0200

    Infinite scroll in list of board games when adding or editing an event

---
+++ boardgame-ui/src/components/user/UserLists.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+<div class="columns is-multiline is-mobile">
+  <div class="column is-one-quarter" v-for="list in lists" v-bind:key="list.id">
+    <div class="card">
+      <div class="card-content">
+        {{list.name}}
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</template>
+
+<script>
+// import BoardGameList from '@/components/board_games/BoardGameList';
+import User from '@/utils/api/User';
+
+export default {
+  props: {
+    
+  },
+  components: {
+    // BoardGameList
+  },
+  data() {
+    return {
+      lists: []
+    };
+  },
+  computed: {
+  },
+  methods: {
+    
+  },
+  async created() {
+    this.lists = await User.fetchCurrentUserLists();
+  }
+};
+</script>
